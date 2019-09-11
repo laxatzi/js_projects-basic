e78939a1efd9e95764d382104d06b833970d9d95
laxatzi <laxatzi@yahoo.com>
2019-09-11 09:49:09 +0300

    create an object

--- simpleObjects.js
+++ simpleObjects.js
@@ -1,3 +1,24 @@
 // simple js exercises regarding objects
 
 
+/*
+Make an object that represents a dog called myDog, which contains the properties "name"(a string), "legs", "tails"and "friends".
+You can set these object properties to whatever values you want, as long "name"is a string, "legs"and "tails"are numbers, and "friends"is an array.
+
+*/
+
+var myDog = {
+   "name" : "Rocky",
+   "legs": 4,
+   "tails": 1,
+   "friends": [
+      "Diego",
+      "Lamby",
+      "Arkas",
+      "Me"
+   ]
+}
+
+
+
+

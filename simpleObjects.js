364c884021a3fd54bef5759f042b999f251bf5f2
laxatzi <laxatzi@yahoo.com>
2019-09-11 11:12:02 +0300

    resolve exercise

--- simpleObjects.js
+++ simpleObjects.js
@@ -30,24 +30,34 @@ function phoneticLookup(val) {
 
    // Switch statement
 
-    switch(val){
-       case "alpha":
-       result= "Adams";
-       break;
-       case "bravo":
-          result = "Boston";
-          break;
-      case "charlie" :
-      result= "Chicago";
-      break;
-      case "delta":
-         result = "Denver"
-         break;
-      case "echo":
-         result = "Easy";
-         break;
-      case "foxtrot":
-         result = "Frank";
-    }
+   //  switch(val){
+   //     case "alpha":
+   //     result= "Adams";
+   //     break;
+   //     case "bravo":
+   //        result = "Boston";
+   //        break;
+   //    case "charlie" :
+   //    result= "Chicago";
+   //    break;
+   //    case "delta":
+   //       result = "Denver"
+   //       break;
+   //    case "echo":
+   //       result = "Easy";
+   //       break;
+   //    case "foxtrot":
+   //       result = "Frank";
+   //  }
+
+      var lookup = {
+         "alpha":"Adams",
+         "bravo": "Boston",
+         "charlie": "Chicago",
+         "delta": "Denver",
+         "echo": "Easy",
+         "foxtrot": "Frank"
+      }
+      result = lookup[val];
    return result;
 }

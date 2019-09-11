b4b53effa5aa48229dce34643250a5c53d931a5b
laxatzi <laxatzi@yahoo.com>
2019-09-11 16:26:22 +0300

    seting an exercise about accessing nested objects

--- simpleObjects.js
+++ simpleObjects.js
@@ -114,3 +114,23 @@ var myMusic = [
    }
 
 ]
+
+//Accessing Nested Object
+
+/*Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable. Use bracket notation for properties with a space in their name.
+*/
+
+//set up
+var myStorage = {
+   "car" : {
+         "inside":{
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+          },
+          "outside" : {
+             "trunk": "jack"
+          }
+      }
+}
+
+var gloveBoxContents = undefined;

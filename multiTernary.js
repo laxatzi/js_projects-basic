d174b9ae0dedf59c49c016e8c6383d71acd86551
laxatzi <laxatzi@yahoo.com>
2019-09-12 22:00:55 +0300

    set up the exercise

--- multiTernary.js
+++ multiTernary.js
@@ -1,1 +1,16 @@
+// Use Multiple Conditional (Ternary) Operators
+
+// Use multiple conditional operators in the checkSign function to check if a number is positive, negative or zero.
+
+function checkSign(num) {
+   if(num > 0){
+     return "number is positive";
+   }
+   else if(num === 0){
+     return "number is zero";
+   }
+   else {
+     return "number is negative";
+   }
+ }
 

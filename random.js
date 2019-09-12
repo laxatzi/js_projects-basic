5c6e23bf98faedcbc3011430fe551baa5e6b9723
laxatzi <laxatzi@yahoo.com>
2019-09-12 19:02:28 +0300

    set up the exercise

--- random.js
+++ random.js
@@ -1,1 +1,8 @@
+//Generate Random Whole Numbers within a Range
+// We use the javascript formula for this : 
+//Math.floor(Math.random() * (max - min + 1)) + min
+
+
+/* Create a function called randomRange that takes a range myMin and myMax and returns a random number that's greater than or equal to myMin, and is less than or equal to myMax, inclusive.
+*/
 

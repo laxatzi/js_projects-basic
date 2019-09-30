0992e64540e426fbf3ec86a9f2dcf88eeb812ef6
laxatzi <laxatzi@yahoo.com>
2019-09-30 11:57:27 +0300

    add comment about reduce function

--- higherOrderFuncs.js
+++ higherOrderFuncs.js
@@ -2,3 +2,10 @@
 JavaScript Array map() Method
 The map() method creates a new array with the results of calling a function for every array element. The map() method calls the provided function once for each element in an array, in order. Note: map() does not execute the function for array elements without values
 */
+
+/*
+    .reduce()
+Just like .map(), .reduce() also runs a callback for each element of an array. What’s different here is that reduce passes the result of this callback (the accumulator) from one array element to the other.
+The accumulator can be pretty much anything (integer, string, object, etc.) and must be instantiated or passed when calling .reduce()
+SYNTAX: array.reduce(function(total, currentValue), initialValue)
+*/

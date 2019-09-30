689d2f2f6336af709ae1f41673282ff01ca7c678
laxatzi <laxatzi@yahoo.com>
2019-09-30 11:38:34 +0300

    add comment about the map function

--- higherOrderFuncs.js
+++ higherOrderFuncs.js
@@ -1,1 +1,4 @@
-
+/*
+JavaScript Array map() Method
+The map() method creates a new array with the results of calling a function for every array element. The map() method calls the provided function once for each element in an array, in order. Note: map() does not execute the function for array elements without values
+*/

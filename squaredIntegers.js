3853629b04d32b424b6ccfe8b62ebf9b8c6fef99
laxatzi <laxatzi@yahoo.com>
2019-09-30 16:01:49 +0300

    solve exercise in ES5

--- squaredIntegers.js
+++ squaredIntegers.js
@@ -2,14 +2,18 @@
 
 const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 const squareList = (arr) => {
-  "use strict";
-  // change code below this line
-  const squaredIntegers = arr;
-  // change code above this line
-  return squaredIntegers;
+   "use strict";
+   // change code below this line
+   const squaredIntegers = realNumberArray.filter(function(num){
+     return num>0 && num % parseInt(num)===0
+   }).map(function(num){
+     return Math.pow(num, 2);
+   });
+   // change code above this line
+   return squaredIntegers;
 };
 // test your code
 const squaredIntegers = squareList(realNumberArray);
-console.log(squaredIntegers);
+console.log(squaredIntegers);//16, 1764, 36
 
 

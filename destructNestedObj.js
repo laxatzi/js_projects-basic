01918812cfee3e4faa25e02696db37660d97a197
laxatzi <laxatzi@yahoo.com>
2019-10-03 16:16:54 +0300

    solving the exercise

--- destructNestedObj.js
+++ destructNestedObj.js
@@ -14,3 +14,18 @@ const anObj = {
 //  Set up the exercise
 // Use destructuring assignment to obtain max of forecast.tomorrow and assign it to maxOfTomorrow.
 
+const LOCAL_FORECAST = {
+   today: { min: 72, max: 83 },
+   tomorrow: { min: 73.3, max: 84.6 }
+ };
+ 
+ function getMaxOfTmrw(forecast) {
+   "use strict";
+   // change code below this line
+   const{tomorrow: {max: maxOfTomorrow}}=forecast; // we assign to the parameter instead of the Const object so that our function is reusable as it should be...code works with AVG_TEMPERATURES instead
+   // change code above this line
+   return maxOfTomorrow;
+ }
+ 
+ console.log(getMaxOfTmrw(LOCAL_FORECAST)); // should be 84.6
+

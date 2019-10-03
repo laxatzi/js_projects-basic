8ebae33e697fa2c4ea25a357504df453fafd7dc3
laxatzi <laxatzi@yahoo.com>
2019-10-03 16:25:52 +0300

    set the exercise

--- destructFromArrs.js
+++ destructFromArrs.js
@@ -11,7 +11,27 @@ console.log(a, b); // 1, 2
 const [a, b,,, c] = [1, 2, 3, 4, 5, 6];
 console.log(a, b, c); // 1, 2, 5
 
+// Use destructuring assignment to swap the values of firstNum and secondNum so that firstNum receives the value stored in secondNum, and vice versa
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
 
+//SOLUTION 
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
 
 
 

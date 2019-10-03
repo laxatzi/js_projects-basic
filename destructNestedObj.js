7142a0890080d0c39b9ec28b325b10ac263719b6
laxatzi <laxatzi@yahoo.com>
2019-10-03 13:45:32 +0300

    give an example of destructuring from nested objects

--- destructNestedObj.js
+++ destructNestedObj.js
@@ -1,1 +1,12 @@
+// We can similarly destructure nested objects into variables.
 
+// Consider the following code:
+
+const anObj = {
+   start: { x: 5, y: 6},
+   end: { x: 6, y: -9 }
+ };
+ const { start : { x: startX, y: startY }} = anObj;
+ const {end:{x:endX, y:endY}} = anObj;
+ console.log(endX, endY);
+ console.log(startX, startY); // 5, 6

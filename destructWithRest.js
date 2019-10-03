b6d1ffc1762792d1a3956a17b2992dacc9b7f778
laxatzi <laxatzi@yahoo.com>
2019-10-03 16:51:41 +0300

    solving the exercise

--- destructWithRest.js
+++ destructWithRest.js
@@ -13,4 +13,17 @@ Variables a and b take the first and second values from the array. After that, b
 The rest element only works correctly as the last variable in the list. As in, you cannot use the rest operator to catch a subarray that leaves out last element of the original array.
 */
 // EXERCISE
-// Use destructuring assignment with the rest operator to perform an effective Array.prototype.slice()so that arris a sub-array of the original array source with the first two elements omitted.
+// Use destructuring assignment with the rest operator to perform an effective Array.prototype.slice()so that arr is a sub-array of the original array source with the first two elements omitted.
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // change code below this line
+  const [a,b,...arr] = list; // change this
+
+  // change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];

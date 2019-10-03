56028fec47dbc93eaee96d52b198fae8057b94de
laxatzi <laxatzi@yahoo.com>
2019-10-03 13:35:03 +0300

    add comments to the solution

--- destructuring.js
+++ destructuring.js
@@ -3,7 +3,7 @@ const AVG_TEMPERATURES = {
    today: 77.5,
    tomorrow: 79
  };
- 
+ // creating a second object to stress the parameter importance (see bellow)
  const AVG_TEMPS = {
    today: 60,
    tomorrow: 70
@@ -12,7 +12,7 @@ const AVG_TEMPERATURES = {
  function getTempOfTmrw(avgTemperatures) {
    "use strict";
    // change code below this line
-   const {tomorrow:tempOfTomorrow} =avgTemperatures; // change this line
+   const {tomorrow:tempOfTomorrow} =avgTemperatures; // we assign to the parameter instead of the Const object so that our function is reusable as it should be...code works with AVG_TEMPERATURES instead
    // change code above this line
    return tempOfTomorrow;
  }

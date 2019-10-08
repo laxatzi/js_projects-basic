ba7824fc917a2fd6986bf8482eb890302ca2456d
laxatzi <laxatzi@yahoo.com>
2019-10-08 20:15:05 +0300

    set up the exercise

--- classes.js
+++ classes.js
@@ -1,1 +1,2 @@
-
+// create a book class. The constructor accepts the book title and the author.
+// Then  create the respective getters and a title setter in the class, to obtain  the book title, and the author.

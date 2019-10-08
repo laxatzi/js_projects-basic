a31681e7197bc7f625b87c0d8bf4867942838a6b
laxatzi <laxatzi@yahoo.com>
2019-10-08 20:34:03 +0300

    solving the exercise

--- classes.js
+++ classes.js
@@ -1,2 +1,27 @@
 // create a book class. The constructor accepts the book title and the author.
 // Then  create the respective getters and a title setter in the class, to obtain  the book title, and the author.
+class Book{
+   constructor(title, author){
+      this.title = title;
+      this.author = author;
+   }
+
+   // getters 
+   get booktitle(){
+      return this.title;
+   }
+
+   get bookauthor(){
+      return this.author;
+   }
+   // setter
+   set booktitle(updater){
+      this.title = updater;
+   }
+}
+
+nextBook = new Book("La nome de la rosa", "Umberto Echo");
+console.log(nextBook.title); // La nome de la rossa
+console.log(nextBook.author); // Umberto Echo
+nextBook.title = "Baudolino";
+console.log(nextBook.title); // Baudolino

1a643c37702db6defb9fab82c499ef0429357d68
laxatzi <laxatzi@yahoo.com>
2019-10-09 12:50:06 +0300

    commenting on the example

--- classMethods.js
+++ classMethods.js
@@ -1,1 +1,4 @@
+//Class Methods in ES6
 
+//ES6 introduced a shorthand that does not require the keyword function for a function assigned to a method's name. One type of class method is the prototype method, which is available to objects of the class.
+//For Example:

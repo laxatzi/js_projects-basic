199cbc4c5609a0a5fbb7a2cbb816be9b5e8e1f90
laxatzi <laxatzi@yahoo.com>
2019-10-10 16:15:56 +0300

    add meaningfull comments to inheritance2.js file and link it to index.html file

--- inheritance2.js
+++ inheritance2.js
@@ -1,1 +1,19 @@
+//Example of Inheritance with constructor
 
+
+/*
+ Explanation:
+
+- Inside constructor function of child class 
+ techMeet, we have to call super() method to 
+ call the parent constructor first otherwise 
+ JavaScript will throw error.
+
+- super() method is nothing but constructor 
+ function of Parent class.
+
+- super() call is must in constructor of 
+ derived class whether explicit presence
+ of parent constructor exists or not.
+
+*/

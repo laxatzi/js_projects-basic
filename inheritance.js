097f34c5948a0d9d86ff7c0c476a73b645ad8155
laxatzi <laxatzi@yahoo.com>
2019-10-10 13:08:04 +0300

    create class with construct function

--- inheritance.js
+++ inheritance.js
@@ -1,1 +1,18 @@
 
+// #ECMAScript 6: Inheritance
+
+//  But what about inheritance? What about 
+//  accessing the super class?
+
+class Hello {
+   constructor(greet){
+      this.greet = greet;
+   }
+   greeting(){
+      return this.greet;
+   }
+}
+
+greeter = new Hello("Hello");
+console.log(greeter.greet); // output: "Hello"
+

a6ec921d4df864aabe8dd8f7500249e32b7c08a3
laxatzi <laxatzi@yahoo.com>
2019-10-10 17:46:32 +0300

    code example with 'super' and 'static' methods

--- superKeyword.js
+++ superKeyword.js
@@ -30,3 +30,28 @@ console.log(getOrganized.organizer);
 
  // one more example
 
+class Meetup2 {
+      organise(){
+         console.log('Organizing Meetup 2');
+      }
+   static getMeetupFounderDetails(){
+         console.log("Meeup2 founder details");
+   }
+}
+
+class techMeet2 extends Meetup2 {
+   organise() {
+      // super.organise()
+      console.log('Organising techMeet4');
+      super.organise();
+   }
+   static getMeetUpFounderDetails(){
+      console.log('techMeet2 Founder details');
+      super.getMeetupFounderDetails();
+   }
+}
+
+let getOrganized2 = new techMeet2();
+console.log(getOrganized2.organise);
+console.log(techMeet2.getMeetupFounderDetails());
+

2392c7e4d9ad365b6a88af863ba8c2751738fc42
laxatzi <laxatzi@yahoo.com>
2019-10-10 17:15:00 +0300

    example of using the super function

--- superKeyword.js
+++ superKeyword.js
@@ -1,1 +1,14 @@
+class Meetup {
+   constructor(organizer){
+      this.organizer = organizer;
+   }
+}
 
+class techMeet extends Meetup {
+   constructor(organizer){
+      super(organizer);
+   }
+}
+
+let getOrganized = new techMeet("Let's get organized");
+console.log(getOrganized.organizer);

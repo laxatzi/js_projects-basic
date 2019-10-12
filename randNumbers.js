b468c70806af59cfd5b075a906eb39890c2823be
laxatzi <laxatzi@yahoo.com>
2019-10-12 18:26:54 +0300

    add meaninfull comments

--- randNumbers.js
+++ randNumbers.js
@@ -3,3 +3,5 @@
 var rand = Math.random();
 console.log(rand);
 
+// The number the Math.random() method generates is inclusive of 0 (as in, it could sometimes be 0, though I’ve never personally seen that happen), but exclusive of 1 (as in, it will never reach 1).
+

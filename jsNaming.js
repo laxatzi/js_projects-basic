80a6a39ff8f2f8d004cc7ad148e53276f25c018c
laxatzi <laxatzi@yahoo.com>
2019-10-14 18:00:08 +0300

    class naming convensions sample

--- jsNaming.js
+++ jsNaming.js
@@ -17,11 +17,24 @@ var encryption = true; // bad
 var hasEncryption = true; // good
 
 
-// Class naming convention
+// Class naming conventions
 /* 
-    
+    A JavaScript class is declared with a PascalCase in contrast to other JavaScript data structures:
 
 */
+class SoftwareDeveloper {
+   constructor(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+   }
+}
+var me = new SoftwareDeveloper('Robin', 'Wieruch');
 
+
+// Component naming conventions
+/*
+   Components are not everywhere in JavaScript, but commonly found in front-end frameworks like React. 
+   Since a component is instantiated like a JS class, they are widely declared with Pascal Case 
+*/
 
 

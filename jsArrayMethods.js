de2935d763e799493eae8268db222fa055d97ec9
laxatzi <laxatzi@yahoo.com>
2019-10-14 21:06:53 +0300

    example of the filter method

--- jsArrayMethods.js
+++ jsArrayMethods.js
@@ -1,4 +1,4 @@
-const arrOfItems = [
+const items = [
    {name: 'Bike', price: 100},
    {name: 'TV', price: 170},
    {name: 'Album', price: 10},
@@ -6,3 +6,11 @@ const arrOfItems = [
    {name: 'SmartPhone', price: 700},
    {name: 'Laptop', price: 1000}
 ]
+
+const threeDigitItems = items.filter((item)=>{
+   return item.price >=100 && item.price <1000;
+})
+
+console.log(threeDigitItems.length);// 3
+
+

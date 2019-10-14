a41ce157ae0f0af8bfbbe52b5ed3590a909ca528
laxatzi <laxatzi@yahoo.com>
2019-10-14 18:27:32 +0300

    create array of object items

--- jsArrayMethods.js
+++ jsArrayMethods.js
@@ -1,1 +1,8 @@
-
+const arrOfItems = [
+   {name: 'Bike', price: 100},
+   {name: 'TV', price: 170},
+   {name: 'Album', price: 10},
+   {name: 'Book', price: 5},
+   {name: 'SmartPhone', price: 700},
+   {name: 'Laptop', price: 1000}
+]

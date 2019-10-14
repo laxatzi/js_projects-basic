3f4339b88cd1f4c02f3d40dfb6d44a5839fc4a5f
laxatzi <laxatzi@yahoo.com>
2019-10-14 17:17:20 +0300

    examples of boolean naming convenstions

--- jsNaming.js
+++ jsNaming.js
@@ -1,1 +1,20 @@
+// JAVASCRIPT NAMING CONVENTIONS
+
+// Boolean
+/*
+    A prefix like 'is', 'are, or 'has' helps every javascript developer 
+    to distinguish a boolean from another date type by just looking at it
+*/
+
+// examples :
+var visible = true; // bad
+var isVisible = true; // good
+
+var equal = true; // bad
+var areEqual = true;  // good
+
+var encryption = true; // bad 
+var hasEncryption = true; // good
+
+
 

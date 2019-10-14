f5d173d513b9cd6a95193eb441ece8f79d394539
laxatzi <laxatzi@yahoo.com>
2019-10-14 21:17:41 +0300

    example of the map method

--- jsArrayMethods.js
+++ jsArrayMethods.js
@@ -7,10 +7,22 @@ const items = [
    {name: 'Laptop', price: 1000}
 ]
 
+// filter method 
+//filters out all the items we don't want in the new array... items in the old array stay immutable
+
 const threeDigitItems = items.filter((item)=>{
    return item.price >=100 && item.price <1000;
 })
 
 console.log(threeDigitItems.length);// 3
+
+// Map Method
+//This method allows you to take one array and convert it to a new array so all the items in the array looks different 
+
+// let's say we want to get the name of every item in a new array
+const nameOfItems = items.map((item)=>{
+   return item.name;
+})
+console.log(nameOfItems);// ["Bike", "TV", "Album","Book", "SmartPhone", "Laptop"]
 
 

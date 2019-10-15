bbc055ff763dbebce50d432ac6306c883574f7a7
laxatzi <laxatzi@yahoo.com>
2019-10-15 13:14:37 +0300

    example of a promise constructor

--- promises.js
+++ promises.js
@@ -1,1 +1,12 @@
+// Example of a Promise constructor
+
+const myPromise = new Promise(
+   (resolve, reject)=>{
+      if(Math.random() * 100 <= 90){
+         resolve('Hello, promises!');
+      }
+      reject(new Error('Sorry, we failed. Miserably'));
+   }
+);
+
 

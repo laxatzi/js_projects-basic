162cfce403c38c9e7910777eb4488283ae299150
laxatzi <laxatzi@yahoo.com>
2019-10-15 17:12:16 +0300

    example of an async function with callbacks

--- callbacks.js
+++ callbacks.js
@@ -9,3 +9,18 @@
 // usually this happens when we are doing I/O e.g talking to databases, downloading things, reading files etc
 
 // example of a normal function
+var result = multiplyTwoNumbs(5,10);
+console.log(result); // 50
+
+// However, async functions use callbacks and dont return anything right away
+
+var photo = downloadPhoto('http//site.com/cat.gif');
+console.log(photo); // undefined
+
+// in the above senario the gif makes long time to download and pause your programme while downloading
+// instead, you store the code that should run AFTER the download is completed into a callback function.
+function handlePhoto(error, photo){
+   if(error) console.log('Download not possible', error)
+   else console.log('Download finished', photo)
+}
+console.log('Download started');

72828af7b17407f99bc0629c8da12b404b84ae06
laxatzi <laxatzi@yahoo.com>
2019-10-15 17:02:59 +0300

    define what a callback is

--- callbacks.js
+++ callbacks.js
@@ -1,1 +1,11 @@
+// WHAT ARE CALLBACKS?
 
+// Callbacks (call me back later) is just the name of a convention for using js functions 
+//There is not a special thing in javascript language syntactically, it is just a convention
+
+// The work asynchronous - async means "take some time" "something will take place not now but in the future"
+// asynchronous functions use callbacks to take some time to produce their results
+
+// usually this happens when we are doing I/O e.g talking to databases, downloading things, reading files etc
+
+// example of a normal function

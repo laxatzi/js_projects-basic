0b3b16eda6fa225ce4b792c43445dd4958b49dc4
laxatzi <laxatzi@yahoo.com>
2019-10-15 13:27:23 +0300

    comment of the actual code

--- promises.js
+++ promises.js
@@ -8,5 +8,13 @@ const myPromise = new Promise(
       reject(new Error('Sorry, we failed. Miserably'));
    }
 );
+// Constructor accepts a function with two parameters...this function is called an 'executor' and describes 
+// the computation to be done. 
+
+// resolve: marks successful completion of the executor 
+// resolve is a function itself ... when the computation is successful we send the value back
+
+// reject: marks unsuccessful completion of the executor
+// reject is a function itself too ... when the computation is UNsuccessful ... thus fails or encounters an error, we signal that by passing the error object to the reject function
 
 

f966ec4bd8816d9c4939bdc93392dad3e08acc4a
laxatzi <laxatzi@yahoo.com>
2019-10-31 10:55:40 +0200

    update iterators.js and index.html files

--- iterators.js
+++ iterators.js
@@ -9,6 +9,37 @@ const myFavAuthors = [
    "Ioannis Kondilakis"
 ];
 
-for(favAuthor of myFavAuthors){
+for(let favAuthor of myFavAuthors){
    console.log(favAuthor);
 }
+
+// How about looping through a more complex data structure
+/*
+const myFavoriteAuthors = {
+   allAuthors: {
+      greek_fiction:[
+         "Alexandros Papadiamantis",
+         "Andreas Karkavitsas",
+         "Gregorios Xenopoylos",
+         "Ioannis Kondilakis"
+      ],
+      international_fiction:[
+         "Goerge Simenon",
+         "Andrea Camilleri",
+         "Emil Zola",
+         "Frank Kern"
+      ],
+      philosophy:[
+         "Marcus Aurelius",
+         "Aristotle",
+         "Arrianus",
+         "Viktor Frankl"
+      ]
+   }
+}
+
+for (let myFavAuthor of myFavoriteAuthors){
+   console.log(myFavAuthor);
+}
+*/
+// Now i get "TypeError: myFavoriteAuthors is not iterable"

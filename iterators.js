d5be027ffb0009cf67dda1d2240bbe885b128798
laxatzi <laxatzi@yahoo.com>
2019-10-31 10:27:48 +0200

    array with a for of loop

--- iterators.js
+++ iterators.js
@@ -1,1 +1,14 @@
+// Javascript iterators
 
+// Lets say we have the bellow array and want to get back all the individual values in the array for printing them on the screen, manipulating them, or for performing some action on them
+
+const myFavAuthors = [
+   "Alexandros Papadiamantis",
+   "Andreas Karkavitsas",
+   "Gregorios Xenopoylos",
+   "Ioannis Kondilakis"
+];
+
+for(favAuthor of myFavAuthors){
+   console.log(favAuthor);
+}

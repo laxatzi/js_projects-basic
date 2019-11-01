c00b35cd56fa378be89b79662fb73896b2a3ed31
laxatzi <laxatzi@yahoo.com>
2019-11-01 19:36:39 +0200

    example of yield woriking with a generator

--- yield.js
+++ yield.js
@@ -8,3 +8,17 @@ function withYield(a){
 const calculateSix = withYield(6);
 console.log(calculateSix.next().value); // 6
 console.log(calculateSix.next().value); // 36
+
+// Yield returns a function only once end the next time you call the same function it moves on to the next yield statement
+// in generators we always get the object as output. It always has two properties, value and done.
+// value for the returned value and done to show us if the generator has completed its job.
+
+function * generator(){
+   yield 7;
+}
+
+const gen = generator();
+
+console.log(gen.next().value); // 7
+console.log(gen.next().value)//undefined
+console.log(gen.next()); // [object Object]{done: true, value: undefined}

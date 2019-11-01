d31d4b172bfbe08223fa7ccaf5952f60d24eb54c
laxatzi <laxatzi@yahoo.com>
2019-11-01 20:38:20 +0200

    initialize generator

--- generators.js
+++ generators.js
@@ -1,1 +1,11 @@
+// generators are reusable but you need to initialize them first 
 
+// create generator
+function * generator(arg="Nothing"){
+   yield arg;
+}
+
+// initialize generator
+const gen1 = generator(); 
+
+console.log(gen1.next().value);//Nothing

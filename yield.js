c84895872674a041d0c967928c7038c951196d8f
laxatzi <laxatzi@yahoo.com>
2019-11-01 13:50:23 +0200

    update index.html and yield.js files

--- yield.js
+++ yield.js
@@ -1,1 +1,10 @@
+function withYield(a){
+   let b = 5;
+   yield a + b;
+   b=6; // now b is reassigned after the first execution
+   yield a * b;
+}
 
+const calculateSix = withYield(6);
+console.log(calculateSix.next().value); // 6
+console.log(calculateSix.next().value); // 36

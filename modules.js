a621e72b659ad9eac8c7eb1c102f5b937ca737c4
laxatzi <laxatzi@yahoo.com>
2019-11-06 10:53:06 +0200

    commenting on what a js module is

--- modules.js
+++ modules.js
@@ -1,1 +1,9 @@
+// what are modules?
+/*
+ Good authors divide their books into chapters and sections; good programmers divide their code into
+ modules.
+ Like book chapters are clusters of words...modules are just clusters of code.
+Good modules are highly self-contained with distinct functionality.
+They can be shuffled, removed or added when appropriate without disturbing the program as a whole
 
+*/

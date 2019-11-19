ce6d3f8c4daababac35b354fee3e4822952a5756
laxatzi <laxatzi@yahoo.com>
2019-11-19 20:57:21 +0200

    example of map method

--- brad_schiff/hiOrderFuncs.js
+++ brad_schiff/hiOrderFuncs.js
@@ -14,6 +14,7 @@
          }
       }
  //multiplierGen is a higher order function since it return an anonymous function
+ // in js we can set a variable to equal a function 
 
       let doubleMe = multiplierGen(2);
       let tripleMe = multiplierGen(3);
@@ -23,4 +24,36 @@
       console.log(tripleMe(5)); // 15
       console.log(tenTimesMe(5));// 50
 
+// higher order function : map()
+    // example
 
+   let pets = [
+      { 
+         name: "Rocky",
+         age: 2,
+         species: "Dog"
+       },
+       {
+         name : "Milly",
+         age: 5,
+         species: "Dog"
+       },
+       {
+          name: "Zizzel",
+          age: 3,
+          species: "Cat"
+       },
+       {
+          name: "Lukas",
+          age: 5,
+          species: "Cat"
+       }
+   ]
+
+  let myPets =  pets.map(nameOnly);
+
+   function nameOnly(pet){
+      return pet.name;
+   }
+console.log(myPets); // ['Rocky','Milly','Zizzel','Lukas'];
+   

b9d835a98eb579879c440d291dcaa2831a1b8c72
laxatzi <laxatzi@yahoo.com>
2019-11-20 16:47:19 +0200

    submit todo items to todo list

--- brad_schiff/todo.js
+++ brad_schiff/todo.js
@@ -1,7 +1,15 @@
-document.getElementById('todo_item-form').
-addEventListener('submit', (evt)=>{
+// store getElementById values to variables for cleaner code
+let todo_form = document.getElementById('todo_item-form');
+let todo_input = document.getElementById('todo_item');
+let todo_list = document.getElementById('todo_list');
+
+// add submit event listener
+todo_form.addEventListener('submit', (evt)=>{
     evt.preventDefault();
-   alert('hello');
+    createItem(todo_input.value);
 });
-  
-   
+
+// create item to list
+function createItem(obligation){
+   todo_list.insertAdjacentHTML('beforeend', obligation);
+}

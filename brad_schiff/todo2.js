e99702d0ad4cc628e2505a0f30db56d8b47b65a6
laxatzi <laxatzi@yahoo.com>
2019-11-21 10:41:58 +0200

    recreate todo app in todo2.js file

--- brad_schiff/todo2.js
+++ brad_schiff/todo2.js
@@ -1,1 +1,25 @@
-let 
+// store elements in variables
+let todoForm = document.getElementById('todo_item-form');
+let todoItem = document.getElementById('todo_item');
+let todoList = document.getElementById('todo_list');
+
+todoForm.addEventListener('submit', (evt)=>{
+    evt.preventDefault();
+    if(todo_item.value){
+        createTodo(todo_item.value);
+    }else{
+        alert("Please insert something need to be done!");
+    }
+
+});
+
+function createTodo(todo){
+    let somethingTodo = `<li>${todo} <button onclick="deleteElement(this)">Delete</button></li>`;
+    todoList.insertAdjacentHTML('beforeend', somethingTodo);
+    todo_item.value = "";
+    todo_item.value.focus();
+}
+
+function deleteElement(p){
+    p.parentElement.remove();
+}

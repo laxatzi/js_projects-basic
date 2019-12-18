3caf1af5f211db9e1fff496e42d81e57d8b57e40
laxatzi <laxatzi@yahoo.com>
2019-12-18 16:47:44 +0200

    example of using callbacks to sort out arr numbers

--- sortArrs.js
+++ sortArrs.js
@@ -1,1 +1,13 @@
+// Sort method is fine for strings but not so for numbers
+    // e.g
+    let numbers = [4,6,12,18,29, 34];
+    console.log(numbers);
+    console.log(numbers.sort());  //(6) [12, 18, 29, 34, 4, 6]
+   // the reason that the sort() method doesn't work well with number arrs is that it's way of sorting out array numbers is to convert them first to strings.
+   // To sort things out we can use a callback function
+   function sortNumbs(a,b){
+      return a-b;
+   }
 
+   // so now we can insert the sortNumbs() function as a callback
+   console.log(numbers.sort(sortNumbs));

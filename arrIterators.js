d2ee0c19282542cea5a2df8a4f96eea55506d185
laxatzi <laxatzi@yahoo.com>
2019-12-19 11:41:55 +0200

    example of forEach array iteration method

--- arrIterators.js
+++ arrIterators.js
@@ -1,1 +1,12 @@
+// forEach()
+// the forEach() method is an alternative to the more verbose one bellow
 
+var colors = ['red', 'green', 'blue'];
+var newColors = ['orange', 'cyan', 'porfyra'];
+for(var j=0; j<colors.length; j++){
+   console.log("Color at position "+ j +" is color "+colors[j])
+}
+
+newColors.forEach(function(color, index){
+   console.log("Color at position "+ index +" is color "+color);
+})

8fab60044878a897f8856a9460195c6ac497a331
laxatzi <laxatzi@yahoo.com>
2020-01-08 19:20:26 +0200

    update searchExample.html and searchExample.js files

--- searchExample.js
+++ searchExample.js
@@ -6,12 +6,13 @@ const button = form['button'];
 // form events ...events exclusive for forms
   input.addEventListener('focus', function(){console.log('focused!'), false});
   input.addEventListener('blur', function(){console.log('blurred!')}, false);
-//   button.addEventListener('click', function(evt){ console.log('everything ok'); evt.preventDefault();}, false); we can use a name here too
+ // button.addEventListener('click', function(evt){ console.log('everything ok'); evt.preventDefault();}, false); 
 
-  form.addEventListener('submit', checkSubmit, false);
+form.addEventListener('submit', makeHero, false);
 
-  function checkSubmit(evt){
-     alert('Submitted');
-     evt.preventDefault();
-  }
-
+function makeHero(evt){
+   evt.preventDefault(); // prevent form from being submitted
+   let hero = {}; // create an empty object... we gonna add properties later on
+   hero.name = form.name.value; // create a name property based on the name value
+   alert(JSON.stringify(hero)); // convert object to JSON and display in alert box
+}

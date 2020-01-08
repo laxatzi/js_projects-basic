eb9871d8b97ddebb94f55c5930bb6db2e8d3a358
laxatzi <laxatzi@yahoo.com>
2020-01-08 10:35:34 +0200

    storing values

--- events.js
+++ events.js
@@ -1,5 +1,12 @@
 function dosomething(evt){
    console.log("screen: (" + evt.screenX + "," + evt.screenY +"), page: (" + evt.pageX+ ","+ evt.pageY +")");
 }
+function whichButton(evt){
+   console.log(evt.which);
+}
+// addEventListener('click', dosomething);
+//addEventListener('click', whichButton);
 
-addEventListener('click', dosomething);
+const click = document.getElementById('click');
+const dbclick = document.getElementById('dbclick');
+const mouse = document.getElementById('mouse');

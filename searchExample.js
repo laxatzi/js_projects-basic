d5049e433508bfca51479b3ebf046166e7201646
laxatzi <laxatzi@yahoo.com>
2020-01-08 18:29:48 +0200

    submiting a form

--- searchExample.js
+++ searchExample.js
@@ -1,9 +1,17 @@
 // storing elements in variables
 const form = document.forms['search'];
 const input = form['searchBox'];
-const button = form.elements[1];
+const button = form['button'];
 
 // form events ...events exclusive for forms
   input.addEventListener('focus', function(){console.log('focused!'), false});
-  input.addEventListener('blur', function(){console.log('blured!')}, false);
+  input.addEventListener('blur', function(){console.log('blurred!')}, false);
+//   button.addEventListener('click', function(evt){ console.log('everything ok'); evt.preventDefault();}, false); we can use a name here too
+
+  form.addEventListener('submit', checkSubmit, false);
+
+  function checkSubmit(evt){
+     alert('Submitted');
+     evt.preventDefault();
+  }
 

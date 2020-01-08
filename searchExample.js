e0dc2ca08e5cc0998fff1d19734414b6991c07e9
laxatzi <laxatzi@yahoo.com>
2020-01-08 18:12:15 +0200

    examples of form events

--- searchExample.js
+++ searchExample.js
@@ -1,1 +1,9 @@
+// storing elements in variables
+const form = document.forms['search'];
+const input = form['searchBox'];
+const button = form.elements[1];
+
+// form events ...events exclusive for forms
+  input.addEventListener('focus', function(){console.log('focused!'), false});
+  input.addEventListener('blur', function(){console.log('blured!')}, false);
 

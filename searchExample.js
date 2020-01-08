e2a8c3b2afb3d9b08e77c2f35b7272a40bff5a28
laxatzi <laxatzi@yahoo.com>
2020-01-08 19:36:59 +0200

    checkbox input fields examples

--- searchExample.js
+++ searchExample.js
@@ -16,3 +16,16 @@ function makeHero(evt){
    hero.name = form.name.value; // create a name property based on the name value
    alert(JSON.stringify(hero)); // convert object to JSON and display in alert box
 }
+
+// looking through the html doc we see that all checkbox elements have the same name property of 'powers'. so we can iterate over this collection of nodes using a for loop to see if each checkbox is checked.
+
+// Checkbox object has a native checked property that checks if checkbox is checked or not
+ 
+hero.powers = []; // create a powers property for our 'hero' obj that starts as an empty array. we then iterate over each checkbox to see if it's been checked. it it's been we add the value property of the checked checkbox to the powers array using the push method.
+
+for(let i =0; i < form.powers.length; i++){
+   if(form.powers[i].checked) hero.powers.push(form.powers[i].value);
+}
+
+// make immortality a standard power
+document.forms.hero.powers[0].checked = true;// remember that 'forms' is a native method 

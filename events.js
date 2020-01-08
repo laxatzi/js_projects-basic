dd986036ae4fc776c163a1de46327e27e2ce4069
laxatzi <laxatzi@yahoo.com>
2020-01-08 11:10:41 +0200

    click events

--- events.js
+++ events.js
@@ -10,3 +10,19 @@ function whichButton(evt){
 const click = document.getElementById('click');
 const dbclick = document.getElementById('dbclick');
 const mouse = document.getElementById('mouse');
+
+// describe the whole click 'cycle'
+   click.addEventListener('mousedown', function(){
+      console.log('down');
+   });
+   click.addEventListener('click', function(){
+      console.log('click');
+   });
+   click.addEventListener('mouseup', function(){
+      console.log('up');
+   });
+   dbclick.addEventListener('dblclick', highlight);
+
+   function highlight(evt){
+      evt.target.classList.toggle('highlight');
+   }

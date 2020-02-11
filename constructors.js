db44a2053e32f534321072743ee8d7afd939ea47
laxatzi <laxatzi@yahoo.com>
2020-02-11 15:34:30 +0200

    example of a class constructor

--- constructors.js
+++ constructors.js
@@ -1,1 +1,35 @@
+// function Constructor
 
+/* 
+   When you use the 'new' keyword, it creates an instance of a constructor function or constructor class. 
+*/
+
+function Country(name, symbol, color){
+   this.name = name;
+   this.symbol = symbol;
+   this.color = color;
+   this.describe = function(){
+      console.log(`${this.name} has a flag that is ${this.color} with a ${this.symbol} as a symbol`);
+   }
+}
+
+const greece = new Country('Greece', 'white cross', "blue");
+greece.describe();
+
+// instead of a function constructor, we can use a class constructor
+
+class Republic {
+   constructor(name, year, flagColor){
+      this.name = name; 
+      this.year = year;
+      this.flagColor = flagColor;
+   } // end of constructor
+
+      describe(){
+         console.log(`${this.name} republic, founded in ${this.year} and has a ${this.flagColor} flag.`);
+      }
+   
+}
+
+const hellas = new Republic("Greece", 1821, "blue and white");
+hellas.describe();

de83700aa60ba858cbd2d06e5f3d2462d71e68aa
laxatzi <laxatzi@yahoo.com>
2020-02-13 16:43:30 +0200

    example of a closure

--- closures.js
+++ closures.js
@@ -1,1 +1,46 @@
+// A closure keeps a reference to a free variable that was created inside the scope of another function, bit is then kept alive and used in another part of the program.
+
+// function scope
+var outside = "I am a global variable";
+function fun(){
+   var inside = "I am a global variable";
+}
+//console.log(outside); // "I am a global variable";
+//console.log(inside);// Error: inside is not defined;
+
+// we are unable to access the variable inside outside of the function.
+
+// But we can gain access to it using a closure
+// example
+function temple(){
+   var weapons = "Katana, Bo, Shuriken, Yuri";
+
+      function innerSanctum(){
+         var stealth = "Cho Ho, Henso-Jutsu, Shinobi-Iri";
+         return "Ninja Skills "+ weapons + " "+stealth;
+      }
+      return innerSanctum();
+}// end of temple
+
+var ninja = temple(); // When the 'temple()' function is called, it can be assigned to a ninja variable that will have access to the information contained in the 'weapons' and 'stealth' variables outside the 'temple()' function.
+// the 'ninja' variable here, represents a Closure, as it has access to variables created inside the 'temple()' function
+
+console.log(ninja); // Ninja Skills Katana, Bo, Shuriken, Yuri Cho Ho, Henso-Jutsu, Shinobi-Iri
+
+// another Closure Example
+
+function closure(){
+   var inside = "created inside";
+   return inside;
+}
+
+//console.log(inside); // Error: inside in not defined
+
+// but we can  create a closure by invoking the closure function which will return the value of 'inside' and make it available outside the function:
+
+var createdOutside = closure();
+console.log(createdOutside); // created inside; --works fine
+
+
+
 

b8b377dc22467544dc02bfdc59f22d8b2d8a0854
laxatzi <laxatzi@yahoo.com>
2020-02-14 11:40:03 +0200

    example of a 'iife' in iffe.js file

--- iffe.js
+++ iffe.js
@@ -1,1 +1,11 @@
+// Immediately Invoded Function Expressions (IIFE)(pronounced "iffy"), are functions that are invoked as soon as are defined.
+// this is easily achieved by following the below syntax
+(function(){
+   var temp = "world";
+   console.log("Hello "+ temp);
+}());
+
+// the last pair of parenthesis invoke the function. The function has to be made into an expression. We achieve this by putting the whole declaration inside a parenthesis.
+// IIFEs are a good way of performing a task while keeping any variables wrapped up within the scope of the function. So global namespace is not polluted with lots of variable names
+
 

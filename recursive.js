25bc171928d50f5453de8191c930d88ffa881f11
laxatzi <laxatzi@yahoo.com>
2020-02-15 16:33:22 +0200

    example of factorial calculation using recursive function

--- recursive.js
+++ recursive.js
@@ -1,1 +1,16 @@
+// Recursive Functions 
 
+/* 
+   A recursive function is one that invokes itself until a certain condition is met.
+   A common example is a function that calcs the factorial of a number
+*/
+
+function factorial(x){
+   if(x<0) return;
+   if(x === 0) return 1;
+   if(x>0){
+      return x * factorial(x-1);
+   }
+}
+
+console.log(factorial(21)); // 120

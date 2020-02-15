fcea9f0b186a1749834292c9c2f67a1ca0598239
laxatzi <laxatzi@yahoo.com>
2020-02-16 01:35:20 +0200

    update file

--- covitCalc.js
+++ covitCalc.js
@@ -1,1 +1,23 @@
+function double(n){
+   return n *2;
+ }
+ let covitHostNow= 60000;
+ let check = 100
+   for(let i =0; i<=30; i++){
+     check =  0.5 * check;
+    
+ }
 
+ console.log(check);
+
+   for(let j =0; j<=12; j++){
+     covitHostNow = double(covitHostNow);
+  }
+ 
+ //console.log(covitHostNow);
+ 
+ function percent(n,p){
+   return (n*p)/100;
+ }
+ 
+ //console.log(percent(covitHostNow, 2));

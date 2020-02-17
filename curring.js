ed356b920baefec21c8e08b89de2a18632ba4db0
laxatzi <laxatzi@yahoo.com>
2020-02-17 15:30:53 +0200

    example of how a curring function works

--- curring.js
+++ curring.js
@@ -1,1 +1,26 @@
+// Curring 
+
+/* 
+   Curring is a process that involves the partial application of functions. A function is said to be curried when not all arguments have been supplied to the function, so it returns another function  that retains the arguments given and expects the remaining arguments in the future.
+*/
+
+function multiplier(x,y){
+   if(y === undefined){
+      return function(z){
+         return x * z;
+      }
+   }else{
+      return x * y;
+   }
+}
+
+const sixTimes = multiplier(6);
+
+console.log(multiplier(5,6)); // 30
+console.log(sixTimes(5)); // 30
+
+// Curring is useful if you find yourself constantly calling a function with the same argument.
+
+// EXERCISE
+  // create a function to calculate vat tax of 24%
 

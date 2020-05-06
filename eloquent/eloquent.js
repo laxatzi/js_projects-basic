446f433d5f860ee1cc337d1034f4ca7816c64717
laxatzi <laxatzi@yahoo.com>
2020-05-06 16:26:37 +0300

    resolve first challenge in ch3 'functions'

--- eloquent/eloquent.js
+++ eloquent/eloquent.js
@@ -1,22 +1,24 @@
-function printFarmInventory(cows, chickens, pigs) {
+//We want to write a program that prints two numbers: the numbers of cows and chickens on a farm, with the words Cows and Chickens after them and zeros
+
+ function printFarmInventory(cows, chickens, pigs) {
    //cows
    let cowString = String(cows);
    while (cowString.length < 3) {
    cowString = "0" + cowString;
    }
-   console.log(`${cowString} Cows`);
+ //  console.log(`${cowString} Cows`);
    // chickens
    let chickenString = String(chickens);
    while (chickenString.length < 3) {
    chickenString = "0" + chickenString;
    }
-   console.log(`${chickenString} Chickens`);
+ //  console.log(`${chickenString} Chickens`);
    // pigs
    let pigString = String(pigs);
    while(pigString.length < 3){
       pigString = "0" + pigString;
    }
-   console.log(`${pigString} Pigs`);
+  // console.log(`${pigString} Pigs`);
    }
    printFarmInventory(7, 11, 17);
 
@@ -25,7 +27,7 @@ function printFarmInventory(cows, chickens, pigs) {
       while (numberString.length < 3) {
       numberString = "0" + numberString;
       }
-      console.log(`${numberString} ${label}`);
+    //  console.log(`${numberString} ${label}`);
       }
 
   function printFarmInventory(cows, chickens, pigs) {
@@ -53,10 +55,22 @@ function printFarmInventory(cows, chickens, pigs) {
       return string;      
   }
 
-  function printFarmInventory(cows, chickens, pigs) {
+  function printFarmInventory(cows, chickens) {
       console.log(`${zeroPad(cows, 3)} Cows`);
       console.log(`${zeroPad(chickens, 3)}  Chickens`);
-      console.log(`${zeroPad(pigs, 3)} Pigs`);
       }
 
       printFarmInventory(7, 11, 3);
+
+       //Exercises
+
+   {
+      // Minimum
+        // Write a function min that takes two arguments and returns their minimum
+
+         const minOfTwo = (a,b)=> Math.min(a,b);
+
+        console.log(minOfTwo(3,6)); // 3
+   }
+
+   

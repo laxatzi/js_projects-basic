3e6cf6df2fca832f07c3e21d5bc5bfe08a6fa8bd
laxatzi <laxatzi@yahoo.com>
2020-05-06 16:56:24 +0300

    update eloquent.js file

--- eloquent/eloquent.js
+++ eloquent/eloquent.js
@@ -66,11 +66,47 @@
 
    {
       // Minimum
-        // Write a function min that takes two arguments and returns their minimum
+        // Write a function min that takes two arguments and returns their minimum.
+        // use an alternative to Math.min method
 
          const minOfTwo = (a,b)=> Math.min(a,b);
+         console.log(minOfTwo(3,6)); // 3
+         const minimumOfTwo = (a,b)=> a>b?b:a;
+         console.log(minimumOfTwo(-4, -19)); // 
+   }    
 
-        console.log(minOfTwo(3,6)); // 3
-   }
+   {
+      // Recursion
+        // We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
 
+        // -- zero is even
+        // -- one is odd
+        // -- for any other number N, its evenness is the same as N-2
+
+        // Define a recursive function isEven that takes advantage of the above conclusions
+        // The function should accept a single parameter (a positive integer) and return a Boolean
+
+         //Test it on 50 and 75. See how it behaves on -1. Why is that? Can you think of a way to fix this?
    
+         // console.log(isEven(50)); //
+         // console.log(isEven(75)); //
+         // console.log(isEven(-1)); // 
+
+   }
+// example to bear in mind
+
+      // function findSolution(target) {
+      // function find(current, history) {
+      //    if (current == target) {
+      //    return history;
+      //    } else if (current > target) {
+      //    return null;
+      //    } else {
+      //    return find(current + 5, `(${history} + 5)`) ||
+      //             find(current * 3, `(${history} * 3)`);
+      //    }
+      // }
+      // return find(1, "1");
+      // }
+
+      // console.log(findSolution(24));

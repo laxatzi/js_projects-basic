d1fa04ba2bf9f7745c3714ad93aa81ad1a84f8a1
laxatzi <laxatzi@yahoo.com>
2020-05-07 12:17:54 +0300

    comment on exercises in eloquentTest.js file

--- eloquent/eloquentTest.js
+++ eloquent/eloquentTest.js
@@ -1,1 +1,19 @@
+ // Minimum
+        // Write a function min that takes two arguments and returns their minimum.
+        // use an alternative to Math.min method
+ 
+ // Recursion
+        // We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
 
+        // -- zero is even
+        // -- one is odd
+        // -- for any other number N, its evenness is the same as N-2
+
+        // Define a recursive function isEven that takes advantage of the above conclusions
+        // The function should accept a single parameter (a positive integer) and return a Boolean
+
+         //Test it on 50 and 75. See how it behaves on -1. Why is that? Can you think of a way to fix this?
+   
+         // console.log(isEven(50)); //
+         // console.log(isEven(75)); //
+         // console.log(isEven(-1)); // 
